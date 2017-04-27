9c1dbf17324ac849e46a410364107ce8d2474c95
Loosper <boian4o1@gmail.com>
2017-04-27 19:04:23 +0300

    Preparation for entering values into a database

--- crawler.py
+++ crawler.py
@@ -1,13 +1,75 @@
 #!/usr/bin/env python3
 import requests
 import json
-import xml.etree.ElementTree
 import hmac
-from lxml import etree
+import sqlite3 as sqlite
+# from lxml import etree
+import xml.etree.ElementTree as etree
 from urllib import parse
 from datetime import datetime
 from hashlib import sha256
 from base64 import b64encode
+
+
+class AmazonAPIHandler:
+    def __init__(self, settings, extra_parameters={}):
+        self.settings = settings
+        self.parameters = {
+            'Service': 'AWSECommerceService',
+            'Operation': 'ItemSearch',
+            'AWSAccessKeyId': self.settings['access_key_id'],
+            'AssociateTag': self.settings['tracking_id'],
+            **extra_parameters
+        }
+
+    def _print_errors(self):
+        for error in self.response.findall('Items/Request/Errors/'):
+            # use logging instead of simple prints
+            etree.dump(error)
+
+    def sign_request(self):
+        # format of the date:  2014-08-18T12:00:00Z
+        current_time = datetime.utcnow()
+        self.parameters['Timestamp'] = current_time.strftime(
+            '%Y-%m-%dT%H:%M:%SZ'
+        )
+
+        encoded_parameters = [
+            parse.urlencode({key: value}, quote_via=parse.quote)
+            for key, value in self.parameters.items()
+        ]
+        encoded_parameters.sort()
+        # print(encoded_parameters)
+
+        hash_string = (
+            'GET\nwebservices.amazon.co.uk\n/onca/xml\n' +
+            '&'.join(encoded_parameters)
+        ).encode('utf-8')
+
+        hasher = hmac.new(
+            self.settings['secret_key'].encode('utf-8'),
+            msg=hash_string,
+            digestmod=sha256
+        ).digest()
+
+        self.parameters['Signature'] = b64encode(hasher).decode()
+
+    def send_request(self):
+        self.sign_request()
+        response = requests.get(
+            'http://webservices.amazon.co.uk/onca/xml',
+            params=self.parameters
+        )
+        response_data = etree.fromstring(response.text.replace(
+            'http://webservices.amazon.com/AWSECommerceService/2011-08-01', ''
+        ))
+        self.response = response_data
+
+        # print(response.text)
+        self._print_errors()
+
+        # either return or assign to self, not sure which is needed
+        return response_data
 
 
 def load_settings():
@@ -22,57 +84,28 @@ def load_settings():
 
 settings = load_settings()
 
-# example input, generated somwhere else
-parameters = {
-    'Service': 'AWSECommerceService',
-    'Operation': 'ItemSearch',
-    'AWSAccessKeyId': settings['access_key_id'],
-    # 'asdfdsafasd': 'test',
-    'AssociateTag': settings['tracking_id'],
-    'Keywords': ''
-}
+request = AmazonAPIHandler(settings, {
+    'Keywords': 'SSD', 'SearchIndex': 'PCHardware', 'MinPercentageOff': '20'
+})
+# print(request.send_request())
+# etree.dump(request.send_request())
+response = request.send_request()
 
+connection = sqlite.connect('database.sql')
+cursor = connection.cursor()
+query = '''INSERT INTO `items`
+    (`ASIN`, `URL`, `ProductGroup`, `manufacturer`,`item_name`)
+    VALUES (?, ?, ?, ?, ?)
+'''
 
-def sign_request(query_parameters, secret_key):
-    # format of the date:  2014-08-18T12:00:00Z
-    current_time = datetime.utcnow()
-    query_parameters['Timestamp'] = current_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+for item in response.findall('Items/Item'):
+    values = (
+        item.find('ASIN').text,
+        item.find('DetailPageURL').text,
+        *(param.text for param in item.find('ItemAttributes'))
+    )
+    cursor.execute(query, values)
 
-    encoded_parameters = [
-        parse.urlencode({key: value})
-        for key, value in query_parameters.items()
-    ]
-    encoded_parameters.sort()
-
-    hash_string = (
-        'GET\nwebservices.amazon.co.uk\n/onca/xml\n' +
-        '&'.join(encoded_parameters)
-    ).encode('utf-8')
-
-    hasher = hmac.new(
-        secret_key.encode('utf-8'),
-        msg=hash_string,
-        digestmod=sha256
-    ).digest()
-
-    query_parameters['Signature'] = b64encode(hasher).decode()
-
-    return query_parameters
-
-
-def print_errors(Errors=None: xml):
-    for error in Errors:
-        print(etree.dump(error))
-
-
-response = requests.get(
-    'http://webservices.amazon.co.uk/onca/xml',
-    params=sign_request(parameters, settings['secret_key'])
-)
-response_data = etree.fromstring(response.text.replace(
-    'http://webservices.amazon.com/AWSECommerceService/2011-08-01', ''
-))
-
-# print(list(response_data.iter(tag='Arguments')))
-for child in response_data.iter('Errors'):
-    print_errors()
+cursor.execute('SELECT `item_name` FROM `items`')
+print(cursor.fetchall())
+# etree.dump(response)
