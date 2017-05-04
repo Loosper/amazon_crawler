9facfeac7d5a800d59d738d5cb9f83b7dcacef5c
Loosper <boian4o1@gmail.com>
2017-05-04 17:23:41 +0300

    Added BrowseNodeLookup

--- crawler.py
+++ crawler.py
@@ -5,6 +5,9 @@ import hmac
 import sqlite3 as sqlite
 # from lxml import etree
 import xml.etree.ElementTree as etree
+# from xml.dom import minidom
+# may be easier to use
+from re import sub
 from urllib import parse
 from datetime import datetime
 from hashlib import sha256
@@ -16,7 +19,6 @@ class AmazonAPIHandler:
         self.settings = settings
         self.parameters = {
             'Service': 'AWSECommerceService',
-            'Operation': 'ItemSearch',
             'AWSAccessKeyId': self.settings['access_key_id'],
             'AssociateTag': self.settings['tracking_id'],
             **extra_parameters
@@ -27,7 +29,7 @@ class AmazonAPIHandler:
             # use logging instead of simple prints
             etree.dump(error)
 
-    def sign_request(self):
+    def _sign_request(self):
         # format of the date:  2014-08-18T12:00:00Z
         current_time = datetime.utcnow()
         self.parameters['Timestamp'] = current_time.strftime(
@@ -55,16 +57,19 @@ class AmazonAPIHandler:
         self.parameters['Signature'] = b64encode(hasher).decode()
 
     def send_request(self):
-        self.sign_request()
+        self._sign_request()
         response = requests.get(
             'http://webservices.amazon.co.uk/onca/xml',
             params=self.parameters
         )
-        response_data = etree.fromstring(response.text.replace(
-            'http://webservices.amazon.com/AWSECommerceService/2011-08-01', ''
-        ))
+
+        response_data = etree.fromstring(
+            sub(' xmlns="[^"]+"', '', response.text, count=1)
+        )
+
         self.response = response_data
 
+        # etree.dump(response_data)
         # print(response.text)
         self._print_errors()
 
@@ -82,30 +87,87 @@ def load_settings():
     return settings
 
 
-settings = load_settings()
+# BrowseNodeLookup
+def get_node_children(node_id: int):
+    request = AmazonAPIHandler(
+        settings, {'Operation': 'BrowseNodeLookup', 'BrowseNodeId': node_id}
+    )
 
-request = AmazonAPIHandler(settings, {
-    'Keywords': 'SSD', 'SearchIndex': 'PCHardware', 'MinPercentageOff': '20'
-})
-# print(request.send_request())
-# etree.dump(request.send_request())
-response = request.send_request()
+    query = '''
+    INSERT OR REPLACE INTO `browse_nodes`
+        (`node_id`, `Name`, `has_children`)
+    VALUES
+        (?, ?, ?);
+    '''
+    children_number = 0
+    response = request.send_request()
+
+    for child in response.findall(
+        'BrowseNodes/BrowseNode/Children/BrowseNode'
+    ):
+        # etree.dump(child)
+        child_id = child.find('BrowseNodeId').text
+        data = (
+            child_id,
+            child.find('Name').text,
+            get_node_children(child_id)
+        )
+        print
+        cursor.execute(query, data)
+        connection.commit()
+        children_number += 1
+
+    return children_number
+
+
+# ItemSearch
+def get_items(parameters):
+    request = AmazonAPIHandler(settings, parameters)
+
+    response = request.send_request()
+    query = '''
+    INSERT OR IGNORE INTO `items`
+    ({})
+    VALUES ({}); -- (?, ?, ?, ?, ?)
+    '''
+
+    for item in response.findall('Items/Item'):
+        # etree.dump(item)
+        values = {
+            'ASIN': item.find('ASIN').text,
+            'DetailPageURL': item.find('DetailPageURL').text,
+            **{param.tag: param.text for param in item.find('ItemAttributes')}
+        }
+
+        columns = ', '.join(values.keys())
+        placeholder = ':'+', :'.join(values.keys())
+
+        # print(json.dumps(values))
+        cursor.execute(query.format(columns, placeholder), values)
+        connection.commit()
+
+
+settings = load_settings()
 
 connection = sqlite.connect('database.sql')
 cursor = connection.cursor()
-query = '''INSERT INTO `items`
-    (`ASIN`, `URL`, `ProductGroup`, `manufacturer`,`item_name`)
-    VALUES (?, ?, ?, ?, ?)
-'''
 
-for item in response.findall('Items/Item'):
-    values = (
-        item.find('ASIN').text,
-        item.find('DetailPageURL').text,
-        *(param.text for param in item.find('ItemAttributes'))
-    )
-    cursor.execute(query, values)
 
-cursor.execute('SELECT `item_name` FROM `items`')
-print(cursor.fetchall())
-# etree.dump(response)
+# ItemLookup for price
+get_items({
+    'Operation': 'ItemSearch', 'Keywords': 'SSD',
+    'SearchIndex': 'PCHardware', 'MinPercentageOff': 20
+})
+# get_node_children(560800)
+
+
+cursor.execute(
+    '''
+    SELECT `node_id`, `Name`, `has_children`
+    FROM `browse_nodes`
+    WHERE `has_children` = 0;
+    '''
+)
+cursor.execute('SELECT `Title` FROM `items`')
+
+print(json.dumps(cursor.fetchall()))
